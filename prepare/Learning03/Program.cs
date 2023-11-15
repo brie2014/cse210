d96030fa24da8aae81028adb63720b5da07cceb3
Brienna Sambrano <brie2014@byui.edu>
2023-11-14 18:26:16 -0700

    week 3 learning

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Setters and getters work
+        Fraction setAndGetFraction = new(8, 13);
+        setAndGetFraction.SetNumerator(5);
+        setAndGetFraction.SetDenominator(10);
+        Console.WriteLine(setAndGetFraction.GetNumerator()); //Should be 5
+        Console.WriteLine(setAndGetFraction.GetDenominator()); //Should be 10
+
+        //Returns string and decimal values for fractions
+        // And test that constructors can handle different scenarios
+        Fraction fraction1 = new();
+        PrintFractionStringAndDecimal(fraction1); // 1/1 and 1
+        Fraction fraction2 = new(5);
+        PrintFractionStringAndDecimal(fraction2); // 5/1 and 5
+        Fraction fraction3 = new(3, 4);
+        PrintFractionStringAndDecimal(fraction3); // 3/4 and 0.75
+        Fraction fraction4 = new(1, 3);
+        PrintFractionStringAndDecimal(fraction4); // 1/3 and 0.3333333
+
+
+    }
+
+    static void PrintFractionStringAndDecimal(Fraction fraction)
+    {
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
