cf3346d9ccc9e4537b205a9069226ff448403280
Brienna Sambrano <brie2014@byui.edu>
2023-11-14 18:51:55 -0700

    prep for team activity

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Scripture
+{
+    // Attributes
+    private Reference _reference;
+    private List<Word> _words;
+
+    // Constructors
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+    }
+
+    //Methods
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+    public string GetDisplayText()
+    {
+
+    }
+    public bool IsCompletelyHidden()
+    {
+
+    }
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Word
+{
+    // Attributes
+    private string _text;
+    private bool _isHidden;
+
+    // Constructors
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    // Methods
+    public void Hide()
+    {
+
+    }
+    public void Show()
+    {
+
+    }
+    public bool IsHidden()
+    {
+    }
+    public string GetDisplayText()
+    {
+    }
+
+}
