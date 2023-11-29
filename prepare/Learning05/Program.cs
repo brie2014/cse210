ed70413988ed6a3c767d71a539d9304737b0d69b
Brienna Sambrano <brie2014@byui.edu>
2023-11-28 22:32:39 -0700

    week 5 learning shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+        Square square = new("purple", 3.5);
+        Rectangle rectangle = new("red", 4, 6.2);
+        Circle circle = new("yellow", 5.3);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
